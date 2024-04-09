9ef2b4c7a3ba788352947aa963c279a8b18d3bb0
ethannance <ethannance523@gmail.com>
2024-04-08 21:50:06 -0600

    nbjntjbtw

--- AuthLab2/Models/ViewModels/ProductsListViewModel.cs
+++ AuthLab2/Models/ViewModels/ProductsListViewModel.cs
@@ -1,16 +1,13 @@
 ﻿namespace AuthLab2.Models.ViewModels
 {
-    // ViewModel for displaying a list of books along with pagination information
     public class ProductsListViewModel
     {
         public IQueryable<Product> Products { get; set; }
         public PaginationInfo PaginationInfo { get; set; } = new PaginationInfo();
         public string? CurrentProductType { get; set; }
-
-        // Add this line
         public IEnumerable<string> ProductTypes { get; set; } = new List<string>();
-
-
+        // Add this line for color filter
+        public IEnumerable<string> ProductColors { get; set; } = new List<string>();
     }
-
 }
+
