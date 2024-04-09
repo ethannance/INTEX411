09078a84203dd487962224a7a4485c2b3a4e89ab
ethannance <ethannance523@gmail.com>
2024-04-08 19:37:46 -0600

    updated database

---
+++ AuthLab2/Components/ProductTypesViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AuthLab2.Models;
+
+namespace AuthLab2.Components
+{
+    public class ProductTypesViewComponent : ViewComponent
+    {
+        private ILegoRepository _productRepo;
+
+        //Constructor
+
+        public ProductTypesViewComponent(ILegoRepository temp) 
+        {
+            _productRepo = temp;
+        }
+        public IViewComponentResult Invoke()
+        {
+            // Attempt to retrieve the bookType from the route values first.
+            string productType = RouteData?.Values["productType"]?.ToString();
+
+            // If not found, try to get it from the query string.
+            if (string.IsNullOrEmpty(productType))
+            {
+                productType = HttpContext.Request.Query["productType"];
+            }
+
+            ViewBag.SelectedProductType = productType;
+
+            var productTypes = _productRepo.Products
+                .Select(x => x.category)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return View(productTypes);
+        }
+
+    }
+}
--- AuthLab2/Models/ViewModels/ProductsListViewModel.cs
+++ AuthLab2/Models/ViewModels/ProductsListViewModel.cs
@@ -3,10 +3,12 @@
     // ViewModel for displaying a list of books along with pagination information
     public class ProductsListViewModel
     {
-        //Books to display
-        public IQueryable<Product> Products { get; set;}
+        public IQueryable<Product> Products { get; set; }
         public PaginationInfo PaginationInfo { get; set; } = new PaginationInfo();
-
         public string? CurrentProductType { get; set; }
+
+        // Add this line
+        public IEnumerable<string> ProductTypes { get; set; } = new List<string>();
     }
+
 }
