9f6e5468bf57d19fdf30bccbd93eb037905eecfe
ethannance <ethannance523@gmail.com>
2024-04-09 10:31:29 -0600

    fixed some

--- AuthLab2/Program.cs
+++ AuthLab2/Program.cs
@@ -18,7 +18,7 @@ var connectionString = configuration.GetConnectionString("DefaultConnection")
     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseSqlServer(connectionString));
 
 services.AddDatabaseDeveloperPageExceptionFilter();
 
