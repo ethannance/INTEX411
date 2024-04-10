02b7a9c3900a83b0312fb8fca9af06bc103e9107
Brenna Fisher <2.bfisher@gmail.com>
2024-04-10 11:16:15 -0600

    color filtering appearing, not working

--- AuthLab2/Models/ViewModels/ProductsListViewModel.cs
+++ AuthLab2/Models/ViewModels/ProductsListViewModel.cs
@@ -6,6 +6,8 @@
         public PaginationInfo PaginationInfo { get; set; } = new PaginationInfo();
         public string? CurrentProductType { get; set; }
         public IEnumerable<string> ProductTypes { get; set; } = new List<string>();
+        public string? CurrentProductColor { get; set; }
+        public IEnumerable<string> ProductColors { get; set; } = new List<string>();
 
     }
 }
