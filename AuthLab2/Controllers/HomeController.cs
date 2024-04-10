beb7d1480f3914d996ce9b237ac5a0b24000e3d8
Brenna Fisher <2.bfisher@gmail.com>
2024-04-10 15:12:25 -0600

    beginning

--- AuthLab2/Controllers/HomeController.cs
+++ AuthLab2/Controllers/HomeController.cs
@@ -131,6 +131,11 @@ namespace AuthLab2.Controllers
             return View();
         }
 
+        public IActionResult ProductsAdmin()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
